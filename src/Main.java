464354970757916ce6e7932b6ae9bdf394fbffcc
Zelioforest <veshky@mail.ru>
2024-01-29 07:41:13 +0300

    many mistake

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,41 @@
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            System.out.println(checkUserData("tiger", "set", "set"));
+            System.out.println(checkUserData("tiger", "set", "set"));
+            System.out.println(checkUserData("tiger", "", "1"));
+        } catch (WrongLoginException | WrongPasswordException e){
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("Exit program");
+        }
+    }
+
+
+    public static boolean checkUserData(String login, String Password, String CheckPassword) {
+        if (!cheсkValidString(login) || login.length() > 20 || login.isEmpty()) {
+            throw new WrongLoginException("Incorrect Login");
+        }
+        if (!cheсkValidString(Password) || Password.length() > 20
+                || Password.isEmpty() || !Password.equals(CheckPassword)) {
+            throw new WrongPasswordException("Incorrect Password");
+        }
+        return true;
+    }
+
+    public static boolean cheсkValidString(String string){
+        for (char str : string.toCharArray()){
+            if (!validString(str)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean validString(char s){
+        return (s >= 65 && s <= 90) ||
+                (s >= 97 && s <= 122) ||
+                (s >= 48 && s <= 57) || s == 95;
     }
 }
